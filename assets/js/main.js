ab8633e5d1c3482e00be85dacadd538817b77c74
Austin M <austin.meier@hotmail.com>
2025-11-25 22:08:09 -0500

    contact details update

--- assets/js/main.js
+++ assets/js/main.js
@@ -144,8 +144,8 @@
       strings: typed_strings,
       loop: true,
       typeSpeed: 100,
-      backSpeed: 50,
-      backDelay: 2000,
+      backSpeed: 75,
+      backDelay: 1000,
     });
   }
 
